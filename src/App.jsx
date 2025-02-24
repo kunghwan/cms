ef219a334a4b8806f90326e41cce711168b41045
kunghwan <ysw03031@naver.com>
2025-02-24 09:29:14 +0900

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ const App = () => {
 
   useEffect(() => {
     localStorage.setItem("users", JSON.stringify(users));
- 
+ //! 최초렌더링 시 빈 배열을 부여하면 아래의 아무런 배열이 없는 useEffect에서 users를 빈 배열로 만들어버리는 버그 발생
+//! 초기값을 로컬스토레지 안의 users를 검사하여 주는 방식으로 변경
 
   });
 
