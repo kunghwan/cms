ba99a4f34ce86544c099cc23ae80d6e1879accfb
kunghwan <ysw03031@naver.com>
2025-02-24 09:25:55 +0900

    final Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import StuForm from "./CMS/StuForm.jsx";
 import StuItem from "./CMS/StuItem";
 import { useState, useEffect } from "react";
-import { studentin } from "./database.js";
+
 import "./App.css";
 
 const App = () => {
   // localStorage에서 'users' 데이터를 불러옵니다. 없으면 빈 배열을 기본값으로 사용합니다.
   // const storedUser = JSON.parse(localStorage.getItem("users"));
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(Json.parse(localStorage.getItem('users'))??[]);
+
   useEffect(() => {
     localStorage.setItem("users", JSON.stringify(users));
-    console.log("updated");
+ 
+
   });
 
   useEffect(() => {
